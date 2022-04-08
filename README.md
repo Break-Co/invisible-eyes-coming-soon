d8f0e1f35dfe3c582471e669d53b0d463b0b8d59
Joseph Tharakan <joseph.tharakan2@allianz.com>
2022-04-08 19:57:04 +0530

    feat: to initialise the basic coming soon page

--- README.md
+++ README.md
@@ -1,22 +1,13 @@
-coming-soon
+Invisible EYEs
 ======================
-[![deploy](https://github.com/break-co/coming-soon/workflows/deploy/badge.svg)](https://github.com/break-co/coming-soon/actions?query=workflow%3Adeploy)
-[![license](https://img.shields.io/github/license/break-co/coming-soon)](LICENSE)
-
-A simple coming soon page, built using [create-react-app](https://github.com/facebook/create-react-app).
-
-<p align="center">
-  <img src="./docs/scrot.png" alt="Coming soon page" width="738">
-</p>
-
-**Note:** I created this as an exercise when I was learning React. So please be mindful of many things that are considered "non-optimal".
+“Seekers of the magical wormhole into the Metaverse”
 
 ## Getting started
 
 ### Installation
 
 1. Clone this repository
-2. `cd coming-soon` to go into the project root
+2. `cd invisible-eyes-coming-soon` to go into the project root
 3. `yarn` to install dependencies
 
 ### Running locally
--- src/App.js
+++ src/App.js
@@ -1,52 +1,85 @@
-import React from 'react';
-import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
-import { ReactComponent as IconGithub } from './assets/icons/github.svg';
-import { ReactComponent as IconDiscord } from './assets/icons/discord.svg';
-import { ReactComponent as IconGmail } from './assets/icons/gmail.svg';
-import { ReactComponent as IconMirror } from './assets/icons/mirror.svg';
-import "./App.css";
+import React from 'react'
+import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg'
+import { ReactComponent as IconGithub } from './assets/icons/github.svg'
+import { ReactComponent as IconDiscord } from './assets/icons/discord.svg'
+import { ReactComponent as IconGmail } from './assets/icons/gmail.svg'
+import { ReactComponent as IconMirror } from './assets/icons/mirror.svg'
+
+import InvisibleEyesLogo from './assets/icons/InvisibleLogo.png'
+
+import './App.css'
 
 class App extends React.Component {
   render = () => {
     return (
-      <div className="card">
-        <div className="header">
-          <div className="logo">
-            <a href=".">Invisible EYEs</a>
+      <div className='card'>
+        <div className='header'>
+          <div className='logo'>
+            <a href='.'>Invisible EYEs</a>
           </div>
-          <div className="social">
-            <a href="https://twitter.com/invsbleEyes" title="Twitter" target="_blank" rel="noopener noreferrer">
-              <IconTwitter className="icon" />
+          <div className='social'>
+            <a
+              href='https://twitter.com/invsbleEyes'
+              title='Twitter'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <IconTwitter className='icon' />
             </a>
-            <a href="https://discord.gg/madUctPypY" title="Discord" target="_blank" rel="noopener noreferrer">
-              <IconDiscord className="icon" />
+            <a
+              href='https://discord.gg/madUctPypY'
+              title='Discord'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <IconDiscord className='icon' />
             </a>
-            <a href="https://github.com/break-co/coming-soon" title="GitHub" target="_blank" rel="noopener noreferrer">
-              <IconGithub className="icon" />
+            <a
+              href='https://github.com/break-co/'
+              title='GitHub'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <IconGithub className='icon' />
             </a>
-            <a href="mailto:support@invisibleeyes.art" title="Gmail" target="_blank" rel="noopener noreferrer">
-              <IconGmail className="icon" />
+            <a
+              href='mailto:support@invisibleeyes.art'
+              title='Gmail'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <IconGmail className='icon' />
             </a>
-            <a href="https://mirror.xyz/invisibleeyes.eth" title="Mirror" target="_blank" rel="noopener noreferrer">
-              <IconMirror className="icon" />
+            <a
+              href='https://mirror.xyz/invisibleeyes.eth'
+              title='Mirror'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <IconMirror className='icon' />
             </a>
           </div>
         </div>
-        <div className="content">
-          <div className="title-holder">
-            <h1>“Seekers of the magical wormhole into the Metaverse”</h1>
-            <p>Coming Soon™️ to the metaverse near you ◬</p>
+        <div className='content'>
+          <div className='title-image'>
+            <img src={InvisibleEyesLogo} alt='InvisibleEyes' />
           </div>
-          <a href="mailto:support@invisibleeyes.art">
-            <div className="cta">Reach out</div>
+          <div className='title-holder'>
+            <h1>“Seekers of the magical 🪄 wormhole into the Metaverse”</h1>
+            <p>◬Prepare yourself for an eternal awakening to see through◬</p>
+          </div>
+          <div className="mail-to">
+          <a href='mailto:support@invisibleeyes.art'>
+            <div className='cta'>Reach out</div>
           </a>
+          </div>
         </div>
-        <div className="footer">
-          <span>made with ⚡️ from the EYEs of the Gods</span>
+        <div className='footer'>
+          <span>made with ⚡️ by the Guardians of the 🛡 Metaverse 🛡</span>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
